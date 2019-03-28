78f60dd2a34622d5ea9bf527cd20cd458e93ddb6
ro6i <ruben@redmart.com>
2019-03-28 18:06:46 +0800

    update

--- keymap.c
+++ keymap.c
@@ -6,6 +6,7 @@ extern keymap_config_t keymap_config;
 
 enum custom_layers {
   _QWERTY,
+  _PWERGQ,
   _EXT,
   _CMD,
   _ADJUST,
@@ -15,6 +16,7 @@ enum custom_layers {
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
+  PWERGQ,
   EXT,
   CMD,
   ADJUST,
@@ -39,6 +41,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RSFT, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSPC, \
+  KC_LCTL, KC_LALT, KC_LGUI, CMD,     EXT,         KC_SPACE,     EXT,     CMD,     KC_RGUI, KC_RALT, KC_RCTL \
+),
+
+/* Pwergq
+ * ,-----------------------------------------------------------------------------------.
+ * | Shift|   P  |   W  |   E  |   R  |   G  |   Q  |   U  |   I  |   O  |   T  |Shift |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   Y  |   H  |   J  |   K  |   L  |   ;  |Enter |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Tab  |   X  |   M  |   C  |   V  |   Z  |   B  |   N  |   ,  |   .  |   /  | Bspc |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl | Alt  | GUI  | CMD  | EXT  |    Space    | EXT  | CMD  | GUI  |  Alt | Ctrl |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_PWERGQ] = LAYOUT_planck_mit( \
+  KC_LSFT, KC_P,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RSFT, \
+  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_Y,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+  KC_TAB,  KC_X,    KC_M,    KC_C,    KC_V,    KC_Z,    KC_B,    KC_N,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSPC, \
   KC_LCTL, KC_LALT, KC_LGUI, CMD,     EXT,         KC_SPACE,     EXT,     CMD,     KC_RGUI, KC_RALT, KC_RCTL \
 ),
 
@@ -80,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust
  * ,-----------------------------------------------------------------------------------.
- * |      |  --  |  --  |  --  |  --  |  --  |  --  |  --  |  --  |  --  |  --  |      |
+ * |      |  MOV |  MOU |  --  |  --  |  --  |  --  |  --  |  --  |  --  |  --  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      | Btn1 | Btn2 | Btn3 | Btn4 | Btn5 |  MsL | MsD  | MsU  | MsR  |  --  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -90,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_mit( \
-  _______, MOV,     MOU,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   MOU,     MOV,    _______, \
+  _______, MOV,     MOU,     KC_NO,   KC_NO,   KC_NO,   PWERGQ,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______, \
   _______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO,   _______, \
   KC_NO,   KC_NO,   KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,   KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO,   KC_SYSREQ, \
   _______, _______, _______, _______, _______,       KC_NO,      _______, _______, _______, _______, _______ \
@@ -126,16 +146,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_MOU] = LAYOUT_planck_mit( \
-  -------, MOV,     KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______, \
+  _______, MOV,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______, \
   _______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO,   _______, \
   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO,   KC_NO, \
   _______, _______, _______, KC_NO,   KC_NO,       UNWIND,       KC_NO,   KC_NO,   _______, _______, _______ \
-),
+)
+
 };
 
 #define NO_CONTROL 0
 
 int control_layer = NO_CONTROL;
+int default_layer = _QWERTY;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
@@ -192,6 +214,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       layer_off(_EXT);
       layer_off(_CMD);
       layer_on(_QWERTY);
+    }
+    return false;
+  case PWERGQ:
+    if (record->event.pressed) {
+      if (default_layer == _QWERTY) {
+        default_layer = _PWERGQ;
+      } else {
+        default_layer = _QWERTY;
+      }
+      default_layer_set(default_layer);
     }
     return false;
   }
