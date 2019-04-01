257e74431f35721c7e469747e81f4b3dfbf08b00
ro6i <ruben@redmart.com>
2019-04-01 17:55:39 +0800

    update

--- keymap.c
+++ keymap.c
@@ -46,38 +46,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Mod1
  * ,-----------------------------------------------------------------------------------.
- * | Shift|   Q  |   W  |   D  |   F  |   M  |   Y  |   U  |   I  |   O  |   P  |Shift |
+ * | Shift|   Q  |   W  |   D  |   F  |  --  |  --  |   U  |   I  |   O  |   P  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Esc  |   A  |   S  |   E  |   R  |   G  |   H  |   J  |   K  |   L  |   T  |Enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Tab  |   ;  |   Z  |   X  |   C  |   V  |   B  |   N  |   ,  |   .  |   /  | Bspc |
+ * | Tab  |   Y  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  | Bspc |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl | Alt  | GUI  | CMD  | EXT  |    Space    | EXT  | CMD  | GUI  |  Alt | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_MOD1] = LAYOUT_planck_mit( \
-  KC_LSFT, KC_Q,    KC_W,    KC_D,    KC_F,    KC_M,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RSFT, \
+  KC_LSFT, KC_Q,    KC_W,    KC_D,    KC_F,    KC_SCLN, KC_SLSH, KC_U,    KC_I,    KC_O,    KC_P,    KC_RSFT, \
   KC_ESC,  KC_A,    KC_S,    KC_E,    KC_R,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_T,    KC_ENT, \
-  KC_TAB,  KC_SCLN, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSPC, \
-  KC_LCTL, KC_LALT, KC_LGUI, CMD,     EXT,         KC_SPACE,     EXT,     CMD,     KC_RGUI, KC_RALT, KC_RCTL \
+  KC_TAB,  KC_Y,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_BSPC, \
+  KC_LCTL, KC_LALT, KC_LGUI, CMD,     EXT,        KC_SPACE,      EXT,     CMD,     KC_RGUI, KC_RALT, KC_RCTL \
 ),
 
-/* Extended/QWERTY keys
+/* Extended keys
  * ,-----------------------------------------------------------------------------------.
  * |      |   1  |   2  |   3  |   4  |      |   `  |   '  |   [  |   ]  |   \  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   5  |   6  |   7  |   8  |   9  |   "  |   _  |   (  |   )  |   :  |      |
+ * |      |   5  |   6  |   7  |   8  |   9  |   "  |   _  |   (  |   )  |   :  |   ;  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |   .  |   0  |   -  |   +  |   *  |   /  |   ~  |   -  |   <  |   >  |   =  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
+ * |      |      |      |      |      |      /      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_EXT] = LAYOUT_planck_mit( \
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_NO,   KC_GRV,  KC_QUOT, KC_LBRC, KC_RBRC, KC_BSLS, _______, \
-  _______, KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_DQUO, KC_UNDS, KC_LPRN, KC_RPRN, KC_COLN, _______, \
+  _______, KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_DQUO, KC_UNDS, KC_LPRN, KC_RPRN, KC_COLN, KC_SCLN, \
   KC_DOT,  KC_0,    KC_PMNS, KC_PPLS, KC_PAST, KC_PSLS, KC_TILD, KC_MINS, KC_LT,   KC_GT,   KC_EQL,  _______, \
-  _______, _______, _______, _______, CMD,          _______,     CMD,     _______, _______, _______, _______ \
+  _______, _______, _______, _______, CMD,          KC_SLSH,     CMD,     _______, _______, _______, _______ \
 ),
 
 /* Command keys
