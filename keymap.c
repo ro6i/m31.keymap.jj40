d211c91e49b76b91b694d3a9eee2d74cbe01e4dc
Robi <ruben@redmart.com>
2019-04-14 23:57:14 +0800

    update

--- keymap.c
+++ keymap.c
@@ -1,12 +1,14 @@
 #include "jj40.h"
 
-#define RGBLIGHT_ANIMATIONS
+/* #define RGBLIGHT_ANIMATIONS */
 
-extern keymap_config_t keymap_config;
+/* extern keymap_config_t keymap_config; */
 
 enum custom_layers {
   _QWERTY,
   _MOD1,
+  _MOD2D,
+  _MOD2A,
   _EXT,
   _CMD,
   _ADJUST,
@@ -16,13 +18,14 @@ enum custom_layers {
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  MOD1,
   EXT,
   CMD,
-  ADJUST,
   MOV,
   MOU,
-  UNWIND
+  UNWIND,
+  DLC_1,
+  DLC_2,
+  DLC_3
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -60,6 +63,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_T,    KC_ENT, \
   KC_TAB,  KC_Y,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_BSPC, \
   KC_LCTL, KC_LALT, KC_LGUI, CMD,     EXT,        KC_SPACE,      EXT,     CMD,     KC_RGUI, KC_RALT, KC_RCTL \
+),
+
+/* Mod2d
+ * ,-----------------------------------------------------------------------------------.
+ * | Shift|      |      |      |      |  --  |  --  |      |      |      |      |Shift |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |      |      |      |      |      |      |      |      |      |      |Enter |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Tab  |      |      |      |      |      |      |      |      |      |      | Bspc |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl | Alt  | GUI  | CMD  | EXT  |    Space    | EXT  | CMD  | GUI  |  Alt | Ctrl |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_MOD2D] = LAYOUT_planck_mit( \
+  KC_LSFT, KC_P,    KC_C,    KC_U,    MO(_MOD2A),   CMD,     CMD,     MO(_MOD2A),   KC_L,    KC_D,    KC_M,    KC_RSFT, \
+  KC_ESC,  KC_A,    KC_S,    KC_E,    KC_T,    KC_G,    KC_H,    KC_I,    KC_O,    KC_N,    KC_R,    KC_ENT, \
+  KC_TAB,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_COMM, KC_DOT,  KC_BSPC, \
+  KC_LCTL, KC_LALT, KC_LGUI, CMD,     EXT,        KC_SPACE,      EXT,     CMD,     KC_RGUI, KC_RALT, KC_RCTL \
+),
+
+/* Mod2a
+ * ,-----------------------------------------------------------------------------------.
+ * | Shift|      |      |      |      |  --  |  --  |      |      |      |      |Shift |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |      |      |      |      |      |      |      |      |      |      |Enter |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Tab  |      |      |      |      |      |      |      |      |      |      | Bspc |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl | Alt  | GUI  | CMD  | EXT  |    Space    | EXT  | CMD  | GUI  |  Alt | Ctrl |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_MOD2A] = LAYOUT_planck_mit( \
+  _______, KC_Q,    KC_Z,    KC_X,    _______, CMD,     CMD,     _______, KC_NO,   KC_NO,   KC_NO,   _______, \
+  _______, KC_Y,    KC_W,    KC_B,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_V,    _______, \
+  _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______, _______, _______, \
+  _______, _______, _______, _______, _______,    _______,       _______, _______, _______, _______, _______ \
 ),
 
 /* Extended keys
@@ -119,7 +158,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_mit( \
-  _______, MOV,     MOU,     KC_NO,   KC_NO,   KC_NO,   MOD1,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______, \
+  _______, MOV,     MOU,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   DLC_1,   DLC_2,   DLC_3,   KC_NO,   _______, \
   _______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO,   _______, \
   KC_NO,   KC_NO,   KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,   KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO,   KC_SYSREQ, \
   _______, _______, _______, _______, _______,       KC_NO,      _______, _______, _______, _______, _______ \
@@ -167,6 +206,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 int control_layer = NO_CONTROL;
 int default_layer = _MOD1;
+/* int default_layer = _MOD2D; */
 
 void keyboard_post_init_user(void) {
   default_layer_set(1UL << default_layer);
@@ -229,13 +269,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       layer_on(_QWERTY);
     }
     return false;
-  case MOD1:
+  /* case MOD1: */
+  /*   if (record->event.pressed) { */
+  /*     if (default_layer == _QWERTY) { */
+  /*       default_layer = _MOD1; */
+  /*     } else { */
+  /*       default_layer = _QWERTY; */
+  /*     } */
+  /*     default_layer_set(1UL << default_layer); */
+  /*   } */
+  /*   return false; */
+  case DLC_1:
     if (record->event.pressed) {
-      if (default_layer == _QWERTY) {
-        default_layer = _MOD1;
-      } else {
-        default_layer = _QWERTY;
-      }
+      default_layer = _QWERTY;
+      default_layer_set(1UL << default_layer);
+    }
+    return false;
+  case DLC_2:
+    if (record->event.pressed) {
+      default_layer = _MOD1;
+      default_layer_set(1UL << default_layer);
+    }
+    return false;
+  case DLC_3:
+    if (record->event.pressed) {
+      default_layer = _MOD2D;
       default_layer_set(1UL << default_layer);
     }
     return false;
