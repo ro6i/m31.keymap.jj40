0a5c3314e69035ddcb5f0934612e75374bf78b0f
ro6i <ruben.chakhmakhchyan@tradegecko.com>
2019-07-19 17:23:44 +0800

    update

--- keymap.c
+++ keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_MOD1] = LAYOUT_planck_mit( \
   KC_LSFT, KC_Q,    KC_W,    KC_D,    KC_F,    x,       KC_Y,    KC_M,    KC_I,    KC_O,    KC_P,    KC_RSFT, \
   KC_ESC,  KC_A,    KC_S,    KC_E,    _V(KC_R),KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_T,    KC_ENT, \
-  KC_TAB,  KC_Z,    KC_Y,    KC_U,    _U(KC_C),KC_V,    KC_B,    KC_N,    KC_COMM, KC_DOT,  KC_X,    KC_BSPC, \
+  KC_TAB,  KC_Z,    KC_Y,    KC_U,    KC_C,    KC_V,    KC_B,    KC_N,    KC_COMM, KC_DOT,  KC_X,    KC_BSPC, \
   KC_LCTL, KC_LALT, KC_LGUI, CMD,     EXT,        KC_SPACE,      EXT,     CMD,     KC_RGUI, KC_RALT, KC_RCTL \
 ),
 
