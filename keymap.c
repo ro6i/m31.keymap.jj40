1ca94497430e46ca2662106280d1121044c1e273
ro6i <ruben@endowus.com>
2021-01-11 16:48:29 +0400

    update

--- keymap.c
+++ keymap.c
@@ -19,6 +19,7 @@ enum custom_keycodes {
   , POINT
   , UNWIND
   , PX_SPACE
+  , PRFX
 };
 
 #define x KC_NO
@@ -31,6 +32,8 @@ enum custom_keycodes {
 
 #define QC_R LT(_QUICK_CONTROL, KC_R)
 #define QC_C LT(_QUICK_POINT, KC_C)
+#define _Q_R _QUICK_CONTROL
+#define _Q_P _QUICK_POINT
 
 #define MC_PRFX LCTL(KC_SPACE)
 
@@ -50,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_MODE1] = LAYOUT_planck_mit( \
   KC_LSFT, KC_Q,    KC_W,    KC_D,    KC_F,    KC_NO,   KC_NO,   KC_M,    KC_I,    KC_O,    KC_P,    KC_RSFT, \
   KC_ESC,  KC_A,    KC_S,    KC_E,    QC_R,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_T,    KC_ENT, \
-  KC_TAB,  KC_Z,    KC_Y,    KC_U,    QC_C,    KC_V,    KC_B,    KC_N,    KC_COMM, KC_DOT,  KC_X,    KC_BSPC, \
+  KC_TAB,  KC_Z,    KC_Y,    KC_U,    KC_C,    KC_V,    KC_B,    KC_N,    KC_COMM, KC_DOT,  KC_X,    KC_BSPC, \
   KC_LCTL, KC_LALT, KC_LGUI, CMD,     EXT,   LCTL_T(KC_SPACE),   EXT,     CMD,     KC_RGUI, KC_RALT, KC_RCTL \
 ),
 
@@ -69,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_1,    KC_2,    KC_3,    KC_4,    x,       x,       KC_DQUO, KC_LBRC, KC_RBRC, KC_BSLS, _______, \
   _______, KC_5,    KC_6,    KC_7,    KC_8,    KC_0,    KC_SCLN, KC_UNDS, KC_LPRN, KC_RPRN, KC_COLN, _______, \
   KC_DOT,  KC_9,    KC_PMNS, KC_PPLS, KC_0,    x,       KC_GRV,  KC_MINS, KC_EQL,  KC_QUOT, KC_SLSH, _______, \
-  _______, _______, _______, _______, MC_PRFX,    PX_SPACE,      MC_PRFX, _______, _______, _______, _______ \
+  _______, _______, _______, _______, PRFX,       PX_SPACE,      PRFX,    _______, _______, _______, _______ \
 ),
 
 /* Command keys
@@ -87,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F13,  x,       DM1_P,   DM2_P,   KC_DEL,  KC_PAUS, _______, \
   _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F14,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS, _______, \
   _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F15,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_APP,  _______, \
-  _______, _______, _______, _______, _______,        x,         _______, _______, _______, _______, _______ \
+  _______, _______, _______, _______, _______,      KC_F16,      _______, _______, _______, _______, _______ \
 ),
 
 /* Adjust
@@ -157,9 +160,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QUICK_CONTROL] = LAYOUT_planck_mit( \
   _______, x,       x,       x,       x,       x,       x,       x,       x,       x,       x,       _______, \
-  _______, KC_LALT, KC_LGUI, x,       _______, x,       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS, _______, \
+  _______, KC_LALT, KC_LGUI, MO(_Q_P),_______, x,       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS, _______, \
   x,       x,       x,       x,       x,       x,       KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DEL,  _______, \
-  _______, _______, _______, x,       KC_LCTL,   _______,        x,       x,       _______, _______, _______ \
+  _______, _______, _______, x,       x,         _______,        x,       x,       _______, _______, _______ \
 ),
 
 /* Quick mouse
@@ -175,9 +178,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QUICK_POINT] = LAYOUT_planck_mit( \
   _______, x,       x,       x,       x,       x,       x,       x,       x,       x,       x,       _______, \
-  _______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN1, _______, \
+  _______, KC_BTN1, KC_BTN2, _______, MO(_Q_R),x,       KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN1, _______, \
   x,       x,       x,       x,       x,       x,       KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, x,       x, \
-  _______, _______, _______, x,       x,           x,            x,       x,       _______, _______, _______ \
+  _______, _______, _______, x,       KC_BTN1,     KC_BTN3,      KC_BTN2, x,       _______, _______, _______ \
 )
 
 };
@@ -236,6 +239,14 @@ process_record_user(uint16_t keycode, keyrecord_t *record) {
         unregister_code(KC_LCTRL);
         is_px_key_on = false;
       }
+    }
+    return false;
+  case PRFX:
+    if (record->event.pressed) {
+      register_code(KC_LCTRL);
+      tap_code(KC_SPACE);
+    } else {
+      unregister_code(KC_LCTRL);
     }
     return false;
   case EXT:
