09779ab17d8a11cf2d0cbb3c7b134fbd9b3b7a33
Ruben Ch8n <ruben@endowus.com>
2023-07-01 02:57:05 +0400

    update

--- keymap.c
+++ keymap.c
@@ -22,16 +22,17 @@ enum custom_keycodes {
   , POINT
   , UNWIND
   /* , PX_SPACE */
-  , PRFX
+  , PF_L
+  , PF_R
 };
 
 #define x KC_NO
 
-#define DM1_R DYN_REC_START1
-#define DM2_R DYN_REC_START2
-#define DM1_P DYN_MACRO_PLAY1
-#define DM2_P DYN_MACRO_PLAY2
-#define DM_STOP DYN_REC_STOP
+/* #define DM1_R DYN_REC_START1 */
+/* #define DM2_R DYN_REC_START2 */
+/* #define DM1_P DYN_MACRO_PLAY1 */
+/* #define DM2_P DYN_MACRO_PLAY2 */
+/* #define DM_STOP DYN_REC_STOP */
 
 #define QC_R LT(_QUICK_CONTROL, KC_R)
 #define QC_C LT(_QUICK_POINT, KC_C)
@@ -75,16 +76,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_EXTL] = LAYOUT_planck_grid( \
   _______, x,       x,       x,       x,       x,       x,       KC_DQUO, KC_LBRC, KC_RBRC, KC_BSLS, KC_SCLN, \
-  _______, KC_5,    KC_6,    MO(_Q_P),MO(_Q_R),x,       x,       KC_UNDS, KC_LPRN, KC_RPRN, KC_COLN, _______, \
-  _______, _______, KC_PMNS, KC_PPLS, KC_0,    x,       KC_GRV,  KC_MINS, KC_EQL,  KC_QUOT, KC_SLSH, _______, \
-  _______, _______, _______, _______, PRFX,    _______, _______, PRFX,    _______, _______, _______, _______ \
+  _______, x,       x,       MO(_Q_P),MO(_Q_R),x,       x,       KC_UNDS, KC_LPRN, KC_RPRN, KC_COLN, _______, \
+  _______, _______, x,       x,       x,       x,       KC_GRV,  KC_MINS, KC_EQL,  KC_QUOT, KC_SLSH, _______, \
+  _______, _______, _______, _______, _______, _______, _______, PF_R,    _______, _______, _______, _______ \
 ),
 
 [_EXTR] = LAYOUT_planck_grid( \
   _______, KC_1,    KC_2,    KC_3,    KC_4,    x,       x,       KC_DQUO, KC_LBRC, KC_RBRC, KC_BSLS, _______, \
   _______, KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    x,       KC_UNDS, KC_LPRN, KC_RPRN, KC_COLN, _______, \
   _______, _______, KC_PMNS, KC_PPLS, KC_DOT,  x,       KC_GRV,  KC_MINS, KC_EQL,  KC_QUOT, _______, _______, \
-  _______, _______, _______, _______, PRFX,    KC_0,    _______, PRFX,    _______, _______, _______, _______ \
+  _______, _______, _______, _______, PF_L,    KC_0,    _______, _______, _______, _______, _______, _______ \
 ),
 
 /* Command keys
@@ -98,8 +99,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |  F16 | F16  |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
+//_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F13,  x,       DM1_P,   DM2_P,   KC_DEL,  KC_PAUS, _______,
 [_CMD] = LAYOUT_planck_grid( \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F13,  x,       DM1_P,   DM2_P,   KC_DEL,  KC_PAUS, _______, \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F13,  x,       x,       x,       KC_DEL,  KC_PAUS, _______, \
   _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F14,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS, _______, \
   _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F15,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_APP,  _______, \
   _______, _______, _______, _______, _______, KC_F16,  KC_F16,  _______, _______, _______, _______, _______ \
@@ -116,10 +118,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
+//_______, DM1_R,   DM2_R,   x,       DM_STOP, x,       x,       x,       x,       x,       x,       _______,
 [_ADJUST] = LAYOUT_planck_grid( \
-  _______, DM1_R,   DM2_R,   x,       DM_STOP, x,       x,       x,       x,       x,       x,       _______, \
+  _______, x,       x,       x,       x,       x,       x,       x,       x,       x,       x,       _______, \
   _______, x,       x,       x,       x,       x,       x,       MOVE,    x,       x,       x,       _______, \
-  x,       x,       KC_MUTE, KC_VOLD, KC_VOLU, x,       x,       POINT,   x,       x,       x,       KC_SYSREQ, \
+  x,       x,       KC_MUTE, KC_VOLD, KC_VOLU, x,       x,       POINT,   x,       x,       x,       KC_SYRQ, \
   _______, _______, _______, _______, _______, x,       x,        _______, _______, _______, _______, _______ \
 ),
 
@@ -201,6 +204,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 int control_layer = NO_CONTROL;
 int default_layer = _MODE1;
+int pfxl_pressed = 0;
+int pfxr_pressed = 0;
+int synthetic_layer_l_on = 0;
+int synthetic_layer_r_on = 0;
 
 int layer_ids[] = {
   _MODE1
@@ -233,13 +240,30 @@ stall_layer(int layer_id_to_turn_on) {
 bool
 process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-  case PRFX:
+  case PF_L:
     if (record->event.pressed) {
-      register_code(KC_LCTRL);
+      register_code(KC_LCTL);
       tap_code(KC_SPACE);
-      unregister_code(KC_LCTRL);
+      unregister_code(KC_LCTL);
+      pfxl_pressed = 1;
     } else {
-      /* unregister_code(KC_LCTRL); */
+      pfxl_pressed = 0;
+      if (synthetic_layer_l_on) layer_off(_EXTL);
+      if (synthetic_layer_r_on) layer_off(_EXTR);
+      synthetic_layer_l_on = 0;
+    }
+    return false;
+  case PF_R:
+    if (record->event.pressed) {
+      register_code(KC_LCTL);
+      tap_code(KC_SPACE);
+      unregister_code(KC_LCTL);
+      pfxr_pressed = 1;
+    } else {
+      pfxr_pressed = 0;
+      if (synthetic_layer_l_on) layer_off(_EXTL);
+      if (synthetic_layer_r_on) layer_off(_EXTR);
+      synthetic_layer_r_on = 0;
     }
     return false;
   case EXTL:
@@ -247,6 +271,10 @@ process_record_user(uint16_t keycode, keyrecord_t *record) {
       layer_on(_EXTL);
     } else {
       layer_off(_EXTL);
+      if (pfxr_pressed) {
+        layer_on(_EXTR);
+        synthetic_layer_r_on = 1;
+      }
     }
     update_tri_layer(_EXTL, _CMD, _ADJUST);
     return false;
@@ -255,6 +283,10 @@ process_record_user(uint16_t keycode, keyrecord_t *record) {
       layer_on(_EXTR);
     } else {
       layer_off(_EXTR);
+      if (pfxl_pressed) {
+        layer_on(_EXTL);
+        synthetic_layer_l_on = 1;
+      }
     }
     update_tri_layer(_EXTR, _CMD, _ADJUST);
     return false;
